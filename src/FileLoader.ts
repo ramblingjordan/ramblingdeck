1d653cedfc1302be4387df8e9b21a0b760db09ec
Jordan Armstrong <ramblingjordan@gmail.com>
2019-11-01 22:00:49 -0400

    generisized tghe array file loader

--- src/FileLoader.ts
+++ src/FileLoader.ts
@@ -10,13 +10,17 @@ export class FileLoader {
 
   loadKeysFile (layoutPath: string): Keys {
     let keys: Keys = {}
-    const yaml = require('js-yaml')
-    const fs = require('fs')
-    const doc = yaml.safeLoad(fs.readFileSync(layoutPath, 'utf8'))
+    let doc = this.loadFile(layoutPath)
     doc.forEach((key: Key) => {
       keys[key.id] = key
     })
     return keys
+  }
+
+  loadArrayFile<T> (filePath: string): T[] {
+    const doc = this.loadFile(filePath)
+    let ret: Array<T> = doc
+    return ret
   }
 
   loadFile (filePath: string): any {
@@ -26,21 +30,5 @@ export class FileLoader {
       fs.readFileSync(filePath, 'utf8')
     )
     return doc
-  }
-
-  loadKeyArray<T> (keyArrayFile: string): T[] {
-    let doc = this.loadFile(keyArrayFile)
-    let ret: Array<T> = []
-    return ret
-  }
-
-  loadMapFile (mapFile: string): Array<Map> {
-    const yaml = require('js-yaml')
-    const fs = require('fs')
-    const doc = yaml.safeLoad(
-      fs.readFileSync(mapFile, 'utf8')
-    )
-    let map: Array<Map> = doc
-    return map
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,16 @@ async function main () {
   const chalk = require('chalk')
   const fileLoader = new FileLoader()
 
+  let layout = 'development'
+
   console.log(chalk.black.bgWhite('\n --- Welcome to RamblingDeck --- \n'))
 
   // Load keys and deck
-  let keys: Keys = fileLoader.loadKeysFile('./layouts/development/keys.yml')
+  let keys: Keys = fileLoader.loadKeysFile('./layouts/' + layout + '/keys.yml')
   const sd = new DeckController(keys)
 
   // Set map from file
-  let map: Array<Map> = fileLoader.loadMapFile('./layouts/development/map.yml')
+  let map: Array<Map> = fileLoader.loadArrayFile<Map>('./layouts/' + layout + '/maps/testing.yml')
   sd.mapping = map
   sd.applyMap()
 
