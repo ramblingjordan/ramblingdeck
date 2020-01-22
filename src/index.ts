23850c3b71acfbb0d695b208b60f318251a3ecfb
Zachary Cowen <cowen.30@osu.edu>
2020-01-22 15:24:30 -0500

    made info block more generic

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,19 @@ async function main () {
   const sd = new DeckController()
   const fileLoader = new FileLoader()
 
-  console.clear()
-  console.log('--- Welcome to RamblingDeck ---')
-  console.log('-------------------------------')
-  console.log('|       StreamDeck Info       |')
+  const infoBlockLength = 31
+  const infoTitle = 'StreamDeck Info'
+  const titleSpaces = (infoBlockLength - 2 - infoTitle.length) / 2
   const version = sd.getFirmwareVersion()
   const sn = sd.getSerialNumber()
-  console.log('|  Version: ' + version + ' '.repeat(31 - 12 - version.length - 1) + '|')
-  console.log('|  Serial #: ' + sn + ' '.repeat(31 - 13 - sn.length - 1) + '|')
-  console.log('-------------------------------')
+
+  console.clear()
+  console.log('--- Welcome to RamblingDeck ---')
+  console.log('-'.repeat(infoBlockLength))
+  console.log('|' + ' '.repeat(titleSpaces) + infoTitle + ' '.repeat(titleSpaces) + (infoBlockLength % 2 > 0 ? '' : ' ') + '|')
+  console.log('|  Version: ' + version + ' '.repeat(infoBlockLength - 12 - version.length - 1) + '|')
+  console.log('|  Serial #: ' + sn + ' '.repeat(infoBlockLength - 13 - sn.length - 1) + '|')
+  console.log('-'.repeat(infoBlockLength))
 
   console.log('Loading keys to deck...')
   sd.keys = fileLoader.loadKeysFile('./layouts/' + LAYOUT + '/keys.yml')
