faba90bc214312e3f0c3cfebe2a15f94a00e22a3
Zachary Cowen <cowen.30@osu.edu>
2020-02-10 13:20:24 -0500

    show message if no stream deck detected

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { listStreamDecks } from 'elgato-stream-deck'
 import { DeckController } from './helpers/DeckController'
 import { FileLoader } from './helpers/FileLoader'
 import { KeyMap } from './models/Key'
@@ -5,31 +6,37 @@ import { KeyMap } from './models/Key'
 const LAYOUT = 'development'
 
 async function main () {
-  const sd = new DeckController()
-  const fileLoader = new FileLoader()
+  if (listStreamDecks().length < 1) {
+    console.log('-----------------------');
+    console.log('No StreamDecks detected');
+    console.log('-----------------------');
+  } else {
+    const sd = new DeckController()
+    const fileLoader = new FileLoader()
 
-  const infoBlockLength = 31
-  const infoTitle = 'StreamDeck Info'
-  const titleSpaces = (infoBlockLength - 2 - infoTitle.length) / 2
-  const version = sd.getFirmwareVersion()
-  const sn = sd.getSerialNumber()
+    const infoBlockLength = 31
+    const infoTitle = 'StreamDeck Info'
+    const titleSpaces = (infoBlockLength - 2 - infoTitle.length) / 2
+    const version = sd.getFirmwareVersion()
+    const sn = sd.getSerialNumber()
 
-  console.clear()
-  console.log('--- Welcome to RamblingDeck ---')
-  console.log('-'.repeat(infoBlockLength))
-  console.log('|' + ' '.repeat(titleSpaces) + infoTitle + ' '.repeat(titleSpaces) + (infoBlockLength % 2 > 0 ? '' : ' ') + '|')
-  console.log('|  Version: ' + version + ' '.repeat(infoBlockLength - 12 - version.length - 1) + '|')
-  console.log('|  Serial #: ' + sn + ' '.repeat(infoBlockLength - 13 - sn.length - 1) + '|')
-  console.log('-'.repeat(infoBlockLength))
+    console.clear()
+    console.log('--- Welcome to RamblingDeck ---')
+    console.log('-'.repeat(infoBlockLength))
+    console.log('|' + ' '.repeat(titleSpaces) + infoTitle + ' '.repeat(titleSpaces) + (infoBlockLength % 2 > 0 ? '' : ' ') + '|')
+    console.log('|  Version: ' + version + ' '.repeat(infoBlockLength - 12 - version.length - 1) + '|')
+    console.log('|  Serial #: ' + sn + ' '.repeat(infoBlockLength - 13 - sn.length - 1) + '|')
+    console.log('-'.repeat(infoBlockLength))
 
-  console.log('Loading keys to deck...')
-  sd.keys = fileLoader.loadKeysFile('./layouts/' + LAYOUT + '/keys.yml')
+    console.log('Loading keys to deck...')
+    sd.keys = fileLoader.loadKeysFile('./layouts/' + LAYOUT + '/keys.yml')
 
-  console.log('Loading and applying mapping...')
-  sd.mapping = fileLoader.loadArrayFile<KeyMap>('./layouts/' + LAYOUT + '/maps/testing.yml')
+    console.log('Loading and applying mapping...')
+    sd.mapping = fileLoader.loadArrayFile<KeyMap>('./layouts/' + LAYOUT + '/maps/testing.yml')
 
-  console.log('Starting deck listeners...')
-  sd.startListeners()
+    console.log('Starting deck listeners...')
+    sd.startListeners()
+  }
 }
 
 void main()
